59d9e5ed9338d9c3b1865c5b7b9ce0a215f635d8
Sunilworks <sunil.kumar.sh9217@gmail.com>
2023-08-05 00:50:35 +0530

    images update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,27 @@
 import React from "react";
 import "./Header.css";
-import { useState } from "react";
-
-const imageSlide = [
-  {
-    url: "https://stat4.bollywoodhungama.in/wp-content/uploads/2018/05/Shershaah-4.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_1280.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2016/08/16/17/32/hollywood-sign-1598473_1280.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2020/11/04/05/08/fitness-and-health-5711489_1280.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg",
-  },
-];
 
 function Header() {
-  const [currentState, setCurrentState] = useState(0);
-  const bgImageStyle = {
-    backgroundImage: `url(${imageSlide[currentState].url})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    height: "600px",
-    width: "990px",
-
-  };
-
-  const goToNext = (currentState) =>{
-    console.log(currentState);
-    setCurrentState(currentState);
-  }
-
   return (
-    <div className="container">
-      <div style={bgImageStyle}>
-        <div className="colousel">
-            {
-            
-                imageSlide.map((imageSlider,currentState) => (
-                    <span key={currentState} onClick={() => goToNext(currentState)}></span>
-                ))
-            }
+    <div className="grid-image-container">
+      <div className="grid-images-one ">
+        <div className="grid-images-caption-one">
+          <h2>EXERCISE: Gym helps in Reducing Stress</h2>
+          <h5>Fitness & Health</h5>
+        </div>
+      </div>
+
+      <div className="grid-images-two ">
+        <div className="grid-images-caption-two">
+          <h2>SHERSHAH: Story of a man in Uniform</h2>
+          <h5>Bollywood</h5>
+        </div>
+      </div>
+
+      <div className="grid-images-three ">
+        <div className="grid-images-caption-three">
+          <h2>PROTEIN:Important Source</h2>
+          <h5>Food</h5>
         </div>
       </div>
     </div>
